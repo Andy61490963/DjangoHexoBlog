8c598fd81e88dbdae9362e01340cd290a4b70bed
Andy61490963 <114399710+Andy61490963@users.noreply.github.com>
2023-12-09 03:24:42 +0800

    新增程式碼區塊markdown
    markdown裡的擴展(markdown.extensions.codehilite)
    用Pygments生成pygments.css
    引入網頁

--- Blog/models.py
+++ Blog/models.py
@@ -25,6 +25,7 @@ class homepost(models.Model):
 class archives(models.Model):
     postname = models.CharField('Name', max_length=20)
     post_date = models.DateTimeField(default=datetime.now, blank=True)
+    content = models.OneToOneField('content', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.postname
@@ -35,10 +36,10 @@ class archives(models.Model):
     def formatted_day(self):
         return self.post_date.strftime('%d')  # %d 將日期格式化為01、02、...、31的形式
 
-
+#archives one to one
 class content(models.Model):
     postname = models.CharField('Name',max_length=20)
-    introduction = MDTextField('Introduction',max_length=500)
+    introduction = MDTextField('Introduction',max_length=10000)
     writer = models.CharField('writer',max_length=20 ,null=True)
     website = models.URLField(max_length=250 ,blank=True ,null=True)
     post_date = models.DateTimeField(default=datetime.now ,blank=True)
--- Blog/templatetags/markdown.py
+++ Blog/templatetags/markdown.py
@@ -6,5 +6,24 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
+@stringfilter
 def convert_markdown(value):
-    return markdown.markdown(value)
+    # 启用大量的 Markdown 扩展
+    extensions = [
+        "markdown.extensions.extra",
+        "markdown.extensions.admonition",
+        "markdown.extensions.codehilite",
+        "markdown.extensions.legacy_attrs",
+        "markdown.extensions.legacy_em",
+        "markdown.extensions.meta",
+        "markdown.extensions.nl2br",
+        "markdown.extensions.sane_lists",
+        "markdown.extensions.smarty",
+        "markdown.extensions.toc",
+        "markdown.extensions.wikilinks",
+    ]
+
+    # 转换 Markdown 到 HTML
+    html = markdown.markdown(value, extensions=extensions)
+
+    return html
